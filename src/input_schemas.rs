3a93cb072deb4334f4f4b182352684fa11971e25
antonguzun <aguzun@ibitcy.com>
2022-10-11 18:31:12 +0300

    fix

--- src/input_schemas.rs
+++ src/input_schemas.rs
@@ -1,5 +1,5 @@
 pub fn parse_item_level(raw: &String) -> Result<u32, String> {
-    let mut raw = raw.trim();
+    let raw = raw.trim();
     match raw.trim().parse::<u32>() {
         Ok(level) => match level {
             1..=100 => Ok(level),
