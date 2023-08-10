a1682f323b3df3115c390ad07ffcf82f65d30909
antonguzun <aguzun@ibitcy.com>
2023-08-10 18:28:37 +0300

    comment

--- src/utils/sync_ext.rs
+++ src/utils/sync_ext.rs
@@ -5,6 +5,16 @@ pub trait MutexLockSExt<T: ?Sized> {
 }
 
 impl<T: ?Sized> MutexLockSExt<T> for Mutex<T> {
+    /// Return String error instead of PoisonError.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use lazy_crafter::utils::sync_ext::MutexLockSExt;
+    ///
+    /// let mutex = Mutex::new(0);
+    /// let guarded_value = mutex.lock_s()?;
+    /// ```
     fn lock_s(&self) -> Result<MutexGuard<T>, String> {
         self.lock().map_err(|x| x.to_string())
     }
